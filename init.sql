c4bce9a630c7a93d70be404cdcfd4e98915b21c4
solop_broadsay <solop@broadsay.com>
2023-03-18 00:38:01 +0700

    fix initial sql

--- init.sql
+++ init.sql
@@ -5,7 +5,3 @@ CREATE TABLE tbl_task (
     description text NOT NULL,
     done boolean DEFAULT false
 );
-
--- Indices -------------------------------------------------------
-
-CREATE UNIQUE INDEX tbl_task_pkey ON tbl_task(id int4_ops);
